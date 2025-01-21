b4066dbdb5efb8933f12a9347a8d0958448e1d5d
FPG Schiba <craftzockerlp@gmail.com>
2025-01-21 14:00:00 +0100

    Fixing terraform_data resource

--- main.tf
+++ main.tf
@@ -68,7 +68,7 @@ resource "aws_iam_role_policy_attachment" "lambda_logs" {
 resource "terraform_data" "build" {
   count = local.is_go_build_lambda ? 1 : 0
 
-  triggers = {
+  triggers_replace = {
     dir_sha1    = sha1(join("", [for f in fileset(var.code_dir, "*") : filesha1("${var.code_dir}/${f}")]))
     file_exists = fileexists(local.build_input_file)
   }
